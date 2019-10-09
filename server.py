5b40c5142b1974df9b3b34d235e0daab5a71089b
cdovan <cdovan16@gmail.com>
2019-10-09 09:26:27 +0200

    Cleaning up server code

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ nodes = [
     '192.168.43.185'
 ]
 
+panicAddress = ('', 10000)
+
 class type_t(enum):
     human = 1
     arm = 2
@@ -34,13 +36,29 @@ for i in range(len(nodes)):
     server_address = (nodes[i], 10000)
     print ('connecting to %s port %s' % server_address)
     sock[i].connect(server_address)
+    sock[i].sendall('CONN_INIT')
+    msg = sock[i].recv(16)
+    if msg not 'CONN_ACK':
+        print('Node has not acknowledged new connection! Continuing...')
 
 while True:
     for i in sock:
-        i.sendall('ay send that data')
-        msg = i.recv(16)
-        if msg == 'panic':
-            ALERT
-            panic()
+        if ALERT and i.address not == panicAddress:
+            i.sendall('PANIC_EXTERN')
+        if ALERT and i.address == panicAddress:
+            i.sendall('PANIC_CONT')
         else:
-            print('node is fucked')
+            i.sendall('DATA_REQ')
+
+        msg = i.recv(16)
+        if msg == 'PANIC':
+            ALERT = True
+            panicAddress = i.address
+            print(i.address, ': Alert recieved')
+        elif msg == 'OK':
+            print(i.address, ': OK')
+            if ALERT:
+                i.sendall('')
+                print('Sending external Alert request to node')
+        else:
+            print('Unknown message received from node! Continuing...')
