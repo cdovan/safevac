941a2692c9803e344cdfc5a267b2f22eb9b015e5
bartvanderhoeven <bartvanderhoeven12@gmail.com>
2019-10-21 14:53:45 +0200

    Added ConstructionSite class

--- navigation.py
+++ navigation.py
@@ -1,49 +1,68 @@
 from dijkstar import Graph, find_path
 from math import sqrt
 
-def path_to_exit(graph, s, exits):
-	min_cost = float("inf")
-	min_path = None
+class ConstructionSite:
+	def __init__(self, nodes_filename, edges_filename):
+		self.nodes, self.exits = read_nodes(nodes_filename)
+		self.edges = read_edges(edges_filename, self.nodes)
+		self.graph = construct_graph(self.edges)
 
-	cost_func = lambda u, v, e, prev_e: e['cost']
-	for e in exits:
-		path = find_path(graph, s, e, cost_func=cost_func)
+	def shortest_path_to_exit(self, s):
+		min_cost = float("inf")
+		min_path = None
+
+		cost_func = lambda u, v, e, prev_e: e['cost']
+		for e in self.exits:
+			path = find_path(self.graph, s, e, cost_func=cost_func)
+			
+			if path.total_cost < min_cost:
+				min_cost = path.total_cost
+				min_path = path
 		
-		if path.total_cost < min_cost:
-			min_cost = path.total_cost
-			min_path = path
-	
-	return min_path
+		return min_path
 
-def set_obstacle(graph, v):
-	for u in graph.get_incoming(v):
-		graph.add_edge(v, u, { "cost": float("inf") })
-		graph.add_edge(u, v, { "cost": float("inf") })
+	def set_edge_obstacle(self, u, v):
+		self.graph.add_edge(u, v, { "cost": float("inf") })
+		self.graph.add_edge(v, u, { "cost": float("inf") })
 
-def remove_obstacle(graph, edges, v):
-	for u in graph.get_incoming(v):
-		distance = 0
-
-		if (v, u) in edges:
-			distance = edges[(v, u)]
-		elif (u, v) in edges:
-			distance = edges[(u, v)]
+	def remove_edge_obstacle(self, u, v):
+		if (v, u) in self.edges:
+			distance = self.edges[(v, u)]
+		elif (u, v) in self.edges:
+			distance = self.edges[(u, v)]
 		else:
 			raise Exception("Neither edge " + str((v, u)) + " nor edge " + str((u, v)) + " can be found in given edges.")
 		
-		graph.add_edge(v, u, { "cost": distance })
-		graph.add_edge(u, v, { "cost": distance })
+		self.graph.add_edge(v, u, { "cost": distance })
+		self.graph.add_edge(u, v, { "cost": distance })
+
+	def set_node_obstacle(self, v):
+		for u in self.graph.get_incoming(v):
+			self.set_edge_obstacle(u, v)
+
+	def remove_node_obstacle(self, v):
+		for u in self.graph.get_incoming(v):
+			self.remove_edge_obstacle(u, v)
 
 def read_nodes(filename):
 	f = open(filename, "r")
 	nodes = {}
+	exits = []
 
-	for node_data in f:
-		node = node_data.split(",")
-		nodes[int(node[0])] = { "x": float(node[1]), "y": float(node[2]) }
+	for node_line in f:
+		node_data = node_line.split(",")
+		node_id = int(node_data[0])
+		node_x = float(node_data[1])
+		node_y = float(node_data[2])
+		is_exit = int(node_data[3]) == 1
+
+		nodes[node_id] = { "x": node_x, "y": node_y }
+		
+		if is_exit:
+			exits.append(node_id)
 	
 	f.close()
-	return nodes
+	return nodes, exits
 
 def read_edges(filename, nodes):
 	f = open(filename, "r")
